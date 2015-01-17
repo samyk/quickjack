eb3263f2397848cc068d2f3a57eca2b077c3ce93
Samy Kamkar <git@samy.pl>
2015-01-16 19:48:48 -0800

    added required file

---
+++ quickjack.js
@@ -0,0 +1,83 @@
+function $(v) { return(document.getElementById(v)); }
+function $S(v) { return($(v).style); }  
+function param(name)
+{
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( window.location.href );
+	return (results == null ? "" : results[1]);
+}
+function urlencode3(s)
+{
+	s = encodeURIComponent(s);
+	return s.replace(/~/g,'%7E').replace(/%20/g,'+');
+}
+function urlencode2(str)
+{
+	return escape(str).replace(/\+/g,'%2B').replace(/%20/g, '+').replace(/\*/g, '%2A').replace(/\//g, '%2F').replace(/@/g, '%40');
+}
+function urldecode( str ) {
+    var histogram = {};
+    var ret = str.toString();
+    
+    var replacer = function(search, replace, str) {
+        var tmp_arr = [];
+        tmp_arr = str.split(search);
+        return tmp_arr.join(replace);
+    };
+    
+    // The histogram is identical to the one in urlencode.
+    histogram["'"]   = '%27';
+    histogram['(']   = '%28';
+    histogram[')']   = '%29';
+    histogram['*']   = '%2A';
+    histogram['~']   = '%7E';
+    histogram['!']   = '%21';
+    histogram['%20'] = '+';
+
+    for (replace in histogram) {
+        search = histogram[replace]; // Switch order when decoding
+        ret = replacer(search, replace, ret) // Custom replace. No regexing   
+    }
+    
+    // End with decodeURIComponent, which most resembles PHP's encoding functions
+    ret = decodeURIComponent(ret);
+
+    return ret;
+}
+function urlencode( str ) {
+    var histogram = {}, tmp_arr = [];
+    var ret = str.toString();
+    
+    var replacer = function(search, replace, str) {
+        var tmp_arr = [];
+        tmp_arr = str.split(search);
+        return tmp_arr.join(replace);
+    };
+    
+    // The histogram is identical to the one in urldecode.
+    histogram["'"]   = '%27';
+    histogram['(']   = '%28';
+    histogram[')']   = '%29';
+    histogram['*']   = '%2A';
+    histogram['~']   = '%7E';
+    histogram['!']   = '%21';
+    histogram['%20'] = '+';
+    
+    // Begin with encodeURIComponent, which most resembles PHP's encoding functions
+    ret = encodeURIComponent(ret);
+    
+    for (search in histogram) {
+        replace = histogram[search];
+        ret = replacer(search, replace, ret) // Custom replace. No regexing
+    }
+    
+    // Uppercase for full PHP compatibility
+    return ret.replace(/(\%([a-z0-9]{2}))/g, function(full, m1, m2) {
+        return "%"+m2.toUpperCase();
+    });
+    
+    return ret;
+}// }}}
+
